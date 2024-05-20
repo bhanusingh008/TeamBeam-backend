34ed611a84cd3e633316c67c2878e342c9d9e9fe
bhanu <bhanusingh0605@gmail.com>
2024-05-20 12:28:04 +0530

    cors fix

--- src/main/java/com/kanban/kanban/controller/QueryController.java
+++ src/main/java/com/kanban/kanban/controller/QueryController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
+@CrossOrigin(origins = "https://teambeam.in", allowCredentials = "true")
 public class QueryController {
 
     @Autowired
